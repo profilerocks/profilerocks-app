b268f0b8ce66ea6bb6b4149728d48089efdef64c
Stefan Samson <ss42701@outlook.com>
2026-07-12 17:22:31 +0200

    fix

--- src/app/(dialog)/u/enter/page.jsx
+++ src/app/(dialog)/u/enter/page.jsx
@@ -4,6 +4,7 @@ import { HREF_PRIVACY, HREF_TERMS } from "#src/lib/env";
 import { requestOtpEnterCreation } from "#src/lib/request";
 import IconAgreement from "#src/icons/agreement.svg";
 import IconInfo from "#src/icons/info.svg";
+import Anchor from "#src/ui/anchor";
 import OauthGoogle from "#src/ui/button/oauth/google";
 import FormUserEmail from "#src/ui/form/email";
 // import styles from "./page.module.scss";
@@ -18,26 +19,28 @@ const formButtonChildren = (
 export default function PageUserEnter() {
   return (
     <>
-      <h1 className="text-3xl">Log in or register</h1>
+      <h1 className="mbe-4 text-3xl">Log in or register</h1>
       <FormUserEmail buttonChildren={formButtonChildren} requestOtpCreation={requestOtpEnterCreation}>
-        <p className="">
-          <IconInfo width="1em" />
+        <p className="mx-2.5 my-5 flex items-start gap-2.5 text-sm">
+          <IconInfo className="max-w-5 text-teal-500" />
           <span className="">
             By accessing this platform, you agree to the{" "}
-            <a href={HREF_PRIVACY} target="_blank" rel="privacy-policy">
+            <Anchor href={HREF_PRIVACY} target="_blank" rel="privacy-policy">
               Privacy Policy
-            </a>{" "}
+            </Anchor>{" "}
             and{" "}
-            <span className="">
-              <a href={HREF_TERMS} target="_blank" rel="terms-of-service">
+            <span className="whitespace-nowrap">
+              <Anchor href={HREF_TERMS} target="_blank" rel="terms-of-service">
                 Terms of Service
-              </a>
+              </Anchor>
               .
             </span>
           </span>
         </p>
       </FormUserEmail>
-      <p className="">OR</p>
+      <p className="my-5 flex items-center gap-4 text-sm text-zinc-300 before:h-px before:flex-1 before:bg-zinc-700 after:h-px after:flex-1 after:bg-zinc-700">
+        OR
+      </p>
       <OauthGoogle>Continue with Google</OauthGoogle>
     </>
   );
---
+++ src/ui/anchor/index.jsx
@@ -0,0 +1,18 @@
+/**
+ * @function
+ * @param {React.InputHTMLAttributes<HTMLAnchorElement>} props
+ * @returns {React.ReactNode}
+ */
+export default function Anchor({ children, className: customClassName, ...restProps }) {
+  return (
+    <a
+      {...restProps}
+      className={
+        "whitespace-nowrap text-teal-500 underline decoration-transparent underline-offset-5 transition-all hover:text-teal-400 hover:decoration-current hover:underline-offset-2 active:text-teal-300" +
+        (customClassName ? " " + customClassName : "")
+      }
+    >
+      {children}
+    </a>
+  );
+}
