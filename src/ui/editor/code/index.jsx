54d0cd42ed92ca5bee7f1c82e7427127a0ab06eb
Stefan Samson <ss42701@outlook.com>
2026-08-05 18:34:40 +0200

    refactor

--- src/ui/editor/code/index.jsx
+++ src/ui/editor/code/index.jsx
@@ -4,33 +4,30 @@ import CodeMirror from "@uiw/react-codemirror";
 import { createTheme } from "@uiw/codemirror-themes";
 import { tags as t } from "@lezer/highlight";
 import { fontCode } from "#src/lib/fonts";
-import "./index.module.scss";
 
 /**
- * @typedef {import("@uiw/react-codemirror").BasicSetupOptions} BasicSetupOptions
- * @typedef {import("@uiw/react-codemirror").ReactCodeMirrorProps} ReactCodeMirrorProps
- * @typedef {import("@uiw/react-codemirror").ViewUpdate} ViewUpdate
+ * @import {BasicSetupOptions,ReactCodeMirrorProps,ViewUpdate} from "@uiw/react-codemirror"
  */
 
 /**
- * @typedef {Object} ButtonCustomProps
- * @property {string} value
+ * @typedef {Object} EditorProps
+ * @prop {React.Dispatch<React.SetStateAction<string>>} setValue
  */
 
 const theme = createTheme({
   theme: "dark",
   settings: {
-    background: "var(--color-bg)",
-    caret: "var(--color-highlight)",
+    background: "#000000",
+    caret: "#00d492",
     fontFamily: fontCode.style.fontFamily,
     fontSize: "0.875em",
-    gutterActiveForeground: "var(--color-2)",
-    gutterBackground: "var(--color-bg)",
-    gutterBorder: "var(--color-highlight)",
-    gutterForeground: "var(--color-4)",
+    gutterActiveForeground: "#d4d4d8",
+    gutterBackground: "#000000",
+    gutterBorder: "#00d492",
+    gutterForeground: "#71717a",
     lineHighlight: "transparent",
-    selection: "var(--color-input-hover)",
-    selectionMatch: "var(--color-input-hover)"
+    selection: "#27272a",
+    selectionMatch: "#27272a"
   },
   styles: [
     { tag: [t.standard(t.tagName), t.tagName], color: "#7ee787" },
@@ -41,7 +38,7 @@ const theme = createTheme({
     { tag: [t.string, t.meta, t.regexp], color: "#a5d6ff" },
     { tag: [t.name, t.quote], color: "#7ee787" },
     { tag: [t.heading, t.strong], color: "#d2a8ff", fontWeight: "bold" },
-    { tag: [t.emphasis], color: "#d2a8ff" /*, fontStyle: 'italic'*/ },
+    { tag: [t.emphasis], color: "#d2a8ff" /*, fontStyle: "italic"*/ },
     { tag: [t.deleted], color: "#ffdcd7", backgroundColor: "ffeef0" },
     { tag: [t.atom, t.bool, t.special(t.variableName)], color: "#ffab70" },
     { tag: t.link, textDecoration: "underline" },
@@ -58,9 +55,9 @@ const EDITOR_BASIC_SETUP = Object.freeze({
 });
 
 /**
- * @param {ReactCodeMirrorProps & {
- *  setValue: React.Dispatch<React.SetStateAction<string>>
- * }} props
+ * @function
+ * @param {ReactCodeMirrorProps&EditorProps} props
+ * @return {React.ReactNode}
  */
 export default function EditorCode({ setValue, value, ...props }) {
   /**
